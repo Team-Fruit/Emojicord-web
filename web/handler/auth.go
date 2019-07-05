5e1ae768a5f0eba1cc773fd3c4c40d45794791eb
sjcl <jrhigasinihonn.0601@gmail.com>
2019-07-05 22:55:05 +0900

    error redirect

--- web/handler/auth.go
+++ web/handler/auth.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"os"
 	"net/http"
+	"net/url"
 	"time"
 	"math/rand"
 	"context"
@@ -37,8 +38,6 @@ type JWTClaims struct {
 type User struct {
 	Username      string `json:"username"`
 	Locale        string `json:"locale"`
-	MfaEnabled    bool   `json:"mfa_enabled"`
-	Flags         int    `json:"flags"`
 	Avatar        string `json:"avatar"`
 	Discriminator string `json:"discriminator"`
 	ID            string `json:"id"`
@@ -62,7 +61,7 @@ func (h *handler) Auth(c echo.Context) error {
 
 	state, err := generateState()
 	if err != nil {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 
 	cookie := &http.Cookie{
@@ -82,33 +81,33 @@ func (h *handler) Auth(c echo.Context) error {
 func (h *handler) Callback(c echo.Context) error {
 	cookieState, err := c.Cookie(cookieName)
 	if err != nil {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 	paramState := c.QueryParam("state")
 	if cookieState.Value != paramState {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 
 	if e := c.QueryParam("error"); e != "" {
-		return echo.NewHTTPError(http.StatusUnauthorized, c.QueryParam("error_description"))
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL(c.QueryParam("error"), c.QueryParam("error_description")))
 	}
 
 	config := GetConfig()
 	token, err := config.Exchange(context.Background(), c.QueryParam("code"))
 	if err != nil {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 
 	client := config.Client(context.Background(), token)
 	res, err := client.Get("https://discordapp.com/api/v6/users/@me")
 	if err != nil {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 	
 	// var guilds []Guild
@@ -119,7 +118,7 @@ func (h *handler) Callback(c echo.Context) error {
 	
 	var user User
 	if err := json.Unmarshal(body, &user); err != nil {
-		return err;
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 
 	claims := &JWTClaims{
@@ -137,7 +136,7 @@ func (h *handler) Callback(c echo.Context) error {
 
 	s, err := t.SignedString([]byte(os.Getenv("JWT_SECRET")))
 	if err != nil {
-		return err
+		return c.Redirect(http.StatusSeeOther, createErrorRedirectURL("internal_server_error", "Internal Server Error"))
 	}
 
 	return c.Redirect(http.StatusSeeOther, os.Getenv("LOGIN_URL") + "?token=" + s)
@@ -149,4 +148,11 @@ func generateState() (string, error) {
 		return "", err
 	}
 	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+func createErrorRedirectURL(err string, desc string) string {
+	params := url.Values{}
+	params.Add("error", err)
+	params.Add("error_description", desc)
+	return os.Getenv("LOGIN_URL") + "?" + params.Encode()
 }
