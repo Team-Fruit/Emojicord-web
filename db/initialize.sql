48f64addb6eaf44a671c53a32755f90cf76ba8eb
sjcl <jrhigasinihonn.0601@gmail.com>
2019-07-09 14:09:23 +0900

    expiry timestamp

--- db/initialize.sql
+++ db/initialize.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `emojicord_db`.`users` (
     `avatar`           VARCHAR(32)    NOT NULL,
     `locale`           VARCHAR(16),
     `created_at`       TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at`       TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_login`       TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY ( `id` )
 );
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `emojicord_db`.`users__discord_tokens` (
     `access_token`     VARCHAR(255)    NOT NULL,
     `token_type`       VARCHAR(255)    NOT NULL,
     `refresh_token`    VARCHAR(255)    NOT NULL,
-    `expiry`           INT UNSIGNED    NOT NULL,
+    `expiry`           TIMESTAMP       NOT NULL,
 
     PRIMARY KEY ( `user_id` ),
 
