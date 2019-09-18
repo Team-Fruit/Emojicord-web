f499c071ef569f4b0f58d972261913d23873f785
sjcl <jrhigasinihonn.0601@gmail.com>
2019-09-18 19:09:58 +0900

    insert emoji

--- web/model/emoji.go
+++ web/model/emoji.go
@@ -15,3 +15,63 @@ type (
 		Enabled bool   `db:"is_enabled"`
 	}
 )
+
+func (m *model) AddEmojis(emojis *[]Emoji) (err error) {
+	tx, err := m.db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+
+	for _, emoji := range *emojis {
+		_, err = tx.Exec(`INSERT INTO discord_emojis
+						VALUES (?, ?, ?, ?, ?)
+						ON DUPLICATE KEY UPDATE
+						name = VALUES(name)`,
+			emoji.ID,
+			emoji.GuildID,
+			emoji.UserID,
+			emoji.Name,
+			emoji.Animated)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func (m *model) AddUserEmojis(userEmojis *[]UserEmoji) (err error) {
+	tx, err := m.db.Begin()
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+
+	for _, ue := range *userEmojis {
+		_, err = tx.Exec(`INSERT INTO users__discord_emojis
+						VALUES (?, ?, ?)
+						ON DUPLICATE KEY UPDATE
+						is_enabled = VALUES(is_enabled)`,
+			ue.UserID,
+			ue.EmojiID,
+			ue.Enabled)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
